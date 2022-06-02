51972cc9de7323a400c8b619447a3f22b5fa5b1b
Shen Zhi <1778495384@qq.com>
2022-06-02 16:09:04 +0800

    add log

--- demo/src/main/java/com/helloworld/demo/controller/HomeController.java
+++ demo/src/main/java/com/helloworld/demo/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.helloworld.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/home")
+@Slf4j
 public class HomeController {
 
     @GetMapping("/hello")
     public String hello(){
+        log.info("{}/{} success","home","hello");
         return "hello haha";
     }
 }
